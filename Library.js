273ea5c366e695807d419a3bcf789fd15aaffdef
CROIRE-Aline <niyocroirealine@gmail.com>
2024-02-22 11:00:23 +0200

    Adding  clear visibility to application

--- Library.js
+++ Library.js
@@ -1,9 +1,15 @@
-// declaring an array to hold books
+// declaring an array of objects to hold books
 var books=[ {
     title:'My angel',
     author:'Joe Charles',
     availability:false
-}];
+}
+,{
+    title: 'Get Rich',
+    author:' King Marcus',
+    availability: true
+}
+];
 // function to add book
 const addbook=(title,author,isavailable)=>{
 let newbook={
@@ -11,17 +17,16 @@ let newbook={
     author:author,
     availability:isavailable
 }
-books.push(newbook);
+books.push(newbook);//pushing a new book in books array
 }
 //adding books
 addbook('The adge','Charles',true);
 addbook('The river between','Karm',true);
-
- console.log(books);
+ console.log(books);//displaying books 
+console.log('')
  //function to display list of available book
- let available=[];
-
  const booksAvailable=()=>{
+    let available=[];
     console.log('available books are:')
     for(let i=0; i<books.length; i++){
     if(books[i].availability===true){
@@ -33,7 +38,9 @@ addbook('The river between','Karm',true);
 }
 //displaying available books
 booksAvailable();
-//function to borrow and return book
+console.log("");
+
+//function to borrow book
 const borrow=(booktitle,borrowername)=>{
 
 let exist=books.some((books) => books.title ===booktitle&&books.availability===true);
@@ -55,6 +62,7 @@ borrow('The adge','Karine')
 borrow('The river between','Aline');
 borrow('My angel','Karine');
 console.log(books);//displaying books to see change after borrowing
+console.log("");
 //function to return book
 const returnBook=(booktitle,holdername)=>{
     let borrowed=books.some((books)=>books.title===booktitle&&books.holder===holdername);
